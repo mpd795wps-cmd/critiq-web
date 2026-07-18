d81f5da977bb3a3b916017829fd07978de5b23fd
mpd795wps-cmd <mpd.79.5wps@gmail.com>
2026-07-18 12:28:28 +0900

    Initial Next.js project

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,12 +22,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
-  );
-}
+return (
+  <html
+    lang="ja"
+    className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+  >
+    <body className="min-h-full flex flex-col">
+      {children}
+    </body>
+  </html>
+);
